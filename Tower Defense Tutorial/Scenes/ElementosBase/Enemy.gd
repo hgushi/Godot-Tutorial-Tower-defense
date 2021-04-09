94e01b1f3a90844c9057178722d808eca3a91474
Borabee <debora.prudencio@gmail.com>
2021-04-09 00:21:20 -0300

    Corrige o bug da torre

--- Tower Defense Tutorial/Scenes/ElementosBase/Enemy.gd
+++ Tower Defense Tutorial/Scenes/ElementosBase/Enemy.gd
@@ -1,13 +1,8 @@
 extends PathFollow2D
 
-signal dead
-
 # Declare member variables here. Examples:
 var speed = 20
 var hp = 10
-
-func _ready():
-	self.connect("dead", self.get_parent().get_node("Torre"), "_on_Enemy_dead")
 
 func _physics_process(delta):
 	offset += speed * delta
@@ -15,7 +10,6 @@ func _physics_process(delta):
 		reached_end()
 
 func reached_end():
-	emit_signal("dead", self)
 	queue_free()
 
 func _on_Area2D_area_entered(projetil):
@@ -24,6 +18,5 @@ func _on_Area2D_area_entered(projetil):
 		projetil.queue_free()
 		if hp <= 0:
 			get_parent().get_parent().add_cash(5)
-			emit_signal("dead", self)
 			queue_free()
 			
--- Tower Defense Tutorial/Scenes/ElementosBase/Torre.gd
+++ Tower Defense Tutorial/Scenes/ElementosBase/Torre.gd
@@ -6,9 +6,8 @@ export var intervalo_de_ataque = 1
 export var sprite = Rect2( 119, 323, 16, 16 )
 
 var tipo_de_projetil = load("res://Scenes/ElementosBase/Projétil.tscn")
-var inimigo_alvo = Area2D
+var inimigo_alvo = weakref(null)
 var inimigos_no_alcance = []
-var target_set = false # Variável de passagem que define se há um alvo definido
 
 # Ao entrar em cena, define as propriedades da torre de acordo com seu tipo
 func _ready():
@@ -18,36 +17,25 @@ func _ready():
 
 # A cada frame, se houver um inimigo alvo, inicia o timer de ataque, senão define o alvo
 func _physics_process(delta):
-	if target_set and $AtaqueTimer.is_stopped(): $AtaqueTimer.start()
-	else: _set_Target()
+	if !inimigo_alvo.get_ref():
+		$AtaqueTimer.stop()
+		
+		for area in get_overlapping_areas():
+			if area.is_in_group("mob"):
+				if !inimigo_alvo.get_ref() or area.get_parent().offset > inimigo_alvo.get_ref().offset:
+					inimigo_alvo = weakref(area.get_parent())
+					
+	if $AtaqueTimer.is_stopped() and not !inimigo_alvo.get_ref():
+		$AtaqueTimer.start()
 
-# Se o inimigo alvo deixar de existir, escolhe um novo alvo
-func on_Enemy_dead(inimigo):
-	if inimigo == inimigo_alvo:
-		target_set = false
-		_set_Target()
-
-# Se o inimigo alvo sair do alcance, escolhe um novo alvo
 func _on_Torre_area_exited(area: Area2D):
-	if area.get_parent() == inimigo_alvo:
-		target_set = false
-		_set_Target()
-
-# Define o inimigo alvo, identificando entre os inimigos no alcance qual está mais avançado no caminho
-func _set_Target():
-	for area in get_overlapping_areas():
-		if area.is_in_group("mob"): inimigos_no_alcance.append(area)
-	
-	if !inimigos_no_alcance.empty() and inimigos_no_alcance[0] != null:
-		inimigo_alvo = inimigos_no_alcance[0].get_parent()
-		for inimigo in inimigos_no_alcance:
-			inimigo = inimigo.get_parent() # Pega o PathFollow2D em vez do Area2D identificado na colisão
-			if inimigo.offset > inimigo_alvo.offset: inimigo_alvo = inimigo
-		target_set = true
+#	if area.is_in_group("mob"): inimigos_no_alcance.erase(weakref(area.get_parent()))
+	if (area.get_parent() == inimigo_alvo or
+		area.get_parent() == inimigo_alvo.get_ref()):
+			inimigo_alvo = weakref(null)
 
 # A cada intervalo de ataque determinado, instancia um projetil e atira na direção do inimigo alvo
 func _on_AtaqueTimer_timeout():
-	if target_set:
-		var projetil = tipo_de_projetil.instance()
-		projetil.direction = (inimigo_alvo.position - self.position).normalized()
-		self.add_child(projetil)
+	var projetil = tipo_de_projetil.instance()
+	projetil.direction = (inimigo_alvo.get_ref().position - self.position).normalized()
+	self.add_child(projetil)
